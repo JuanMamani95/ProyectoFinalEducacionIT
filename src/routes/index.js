74178b5d618d3aa17a1f72f3623c927ffe82ac2c
Juan Mamani <juanmamani@hotmail.com>
2023-02-04 00:15:11 -0300

    comienzo del proyecto

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index.hbs');
+});
+
+router.get('/about', (req, res) => {
+    res.render('About.hbs');
+});
+
+router.get('/productos', (req, res) => {
+    res.render('productos.hbs');
+});
+
+module.exports = router;
